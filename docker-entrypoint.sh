3f6593cf7db472e8f3e9c0c5741135e84b23d401
KhaledAly <91162827+ThereAreNoMoreNames@users.noreply.github.com>
2025-11-06 09:39:03 -0500

    dataset updates...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "Python version: $(python --version)"
 echo "UV version: $(uv --version)"
 
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-mkdir -p /mnt/gcs_bucket
+mkdir -p /mnt/gcs_data
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
 mkdir -p /app/cheese_dataset
